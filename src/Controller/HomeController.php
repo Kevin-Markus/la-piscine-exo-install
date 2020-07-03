52180bb745468dece9e4529e3eec04a85835e5eb
David Robert <contact.davidrobert@gmail.com>
2020-07-03 16:17:47 +0200

    récupération d'un parametre d'url avec la classe Request

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,25 +6,39 @@ namespace App\Controller;
 
 // je viens utiliser la classe Route donc j'ai besoin d'importer
 // son namespace
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // je créé une classe HomeController, qui correspond
 // au nom du fichier, et qui me permet de regrouper des routes
-class HomeController {
+// ma classe de controleur étend la classe de Symfony AbstractController
+// ça permet à ma classe de bénéficer des méthodes de la classe
+// AbstractController (c'est le système d'héritage en PHP)
+class HomeController extends AbstractController {
 
     /**
      * @Route("/test2", name="home")
-     *
-     * je créé une route dans un commentaire que PHP va
-     * exécuter (c'est donc une annotation). Je sais que c'est une
-     * annotation car il y une classe avec un @ devant.
-     * J'associe une url à ma route, et je lui associe aussi
-     * une méthode de controleur (en plaçant mon annotation au dessus
-     * de la méthode). La méthode du controleur sera donc executée
-     * quand l'url sera demandée dans un navigateur
      */
-    public function home()
+    // Si je veux récupérer une instance de la classe de
+    // Symfony "Request", je peux la créer avec "new Request()"
+    // Mais aussi je peux utiliser le méchanisme "d'autowire'"
+    // de Symfony qui me permet de spécifier en parametre d'une méthode
+    // la classe à instancier + le nom d'une variable
+    // et symfony automatiquement va instancier la classe dans la variable
+    public function home(Request $request)
     {
-        var_dump('hello home'); die;
+        // j'utilise l'instance de la classe Request
+        // pour récupérer le parametre d'url 'age'
+        // cette classe Request de Symfony sert uniquement à ça :
+        // récupérer des informations de la requête utilisateur (ip, url
+        // méthode, parametres etc)
+        $age = $request->query->get('age');
+
+        var_dump($age); die;
     }
+
 }
+
+
